3a3cf72bbbbb18776dbcfefcc4bbb24f37a1a4ac
belzar <belzar@yandex.ru>
2016-05-29 20:02:49 +0300

    Отступы

--- js/SCT.js
+++ js/SCT.js
@@ -1,105 +1,97 @@
 'use strict'
 ;(function() {
-	
-	var Cards = {
 
-		/**
-		 * Массив для хранения карточек
-		 */
-		_cards: [],
+    var Cards = {
 
-		/**
-		 * Добавляет карточку в массив
-		 * @param {Object} - params - информация о карточке путешественника
-		 * @param params.typeTC - тип транспорта
-		 * @param params.flight - номер рейса
-		 * @param params.seat - посадочное место
-		 * @param params.A - точка отправки
-		 * @param params.B - точка прибытия
-		 */
-		addCard: function(params) {
-			this._cards.push( params );
-		},
+        /**
+         * Массив для хранения карточек
+         */
+        _cards: [],
 
-		/**
-		 * Удаляет все карточки путешественника из массива _cards
-		 */
-		empty: function() {
-			this._cards = [];
-		},
+        /**
+         * Добавляет карточку в массив
+         * @param {Object} - params - информация о карточке путешественника
+         * @param params.typeTC - тип транспорта
+         * @param params.flight - номер рейса
+         * @param params.seat - посадочное место
+         * @param params.A - точка отправки
+         * @param params.B - точка прибытия
+         */
+        addCard: function(params) {
+            this._cards.push( params );
+        },
 
-		/**
-		 * Случайным образом перемешивает массив _cards
-		 */
-		shuffle: function() {
-			this._cards.sort(function(a, b) {
-				return (Math.round(Math.random()) - 0.5);
-			});
-		},
+        /**
+         * Удаляет все карточки путешественника из массива _cards
+         */
+        empty: function() {
+            this._cards = [];
+        },
 
-		/**
-		 * Сортирует массив карточек _cards начиная с начала маршрута.
-		 */
-		sortCards: function() {
-			var index = 0,
-				temp;
+        /**
+         * Случайным образом перемешивает массив _cards
+         */
+        shuffle: function() {
+            this._cards.sort(function(a, b) {
+                return (Math.round(Math.random()) - 0.5);
+            });
+        },
 
-			/**
-			 * Находит точку отправки всего машрута(первую карточку)
-			 */
-			for (var i = 1; i < this._cards.length; i++) {
-				if (this._cards[index].A === this._cards[i].B) {
-					index++;
-					i = -1;
-					continue;
-				}
-			}
+        /**
+         * Сортирует массив карточек _cards начиная с начала маршрута.
+         */
+        sortCards: function() {
+            var index = 0,
+                temp;
 
-			/**
-			 * Карточку с началом пути устанавливает первой в массиве _cards 
-			 */
-			temp = this._cards[index];
-			this._cards[index] = this._cards[0];
-			this._cards[0] = temp;
+            // Находит точку отправки всего машрута(первую карточку)
+            for (var i = 1; i < this._cards.length; i++) {
+                if (this._cards[index].A === this._cards[i].B) {
+                    index++;
+                    i = -1;
+                    continue;
+                }
+            }
 
-			/**
-			 *  Находит карточку, у которой точка прибытия совпадает с точкой отправки текущей карточки 
-			 *  и устанавливает ее следующей за текущей
-			 */
-			for (var i = 1, index = 0; i < this._cards.length; i++) {
-				if (this._cards[index].B === this._cards[i].A) {
-					temp = this._cards[++index];
-					this._cards[index] = this._cards[i];
-					this._cards[i] = temp;
-					i = index;
-				}
-			}
+            // Карточку с началом пути устанавливает первой в массиве _cards 
+            temp = this._cards[index];
+            this._cards[index] = this._cards[0];
+            this._cards[0] = temp;
 
-		},
+            // Находит карточку, у которой точка прибытия совпадает с точкой отправки текущей карточки и устанавливает ее следующей за текущей
+            for (var i = 1, index = 0; i < this._cards.length; i++) {
+                if (this._cards[index].B === this._cards[i].A) {
+                    temp = this._cards[++index];
+                    this._cards[index] = this._cards[i];
+                    this._cards[i] = temp;
+                    i = index;
+                }
+            }
+        },
 
-		/**
-		 * Каждый объект массива _cards преобразует в строк и пушит в новый массив 
-		 * @return {Array} - возвращает преобразованный массив arr 
-		 */
-		conversion: function() {
-			for (var i = 0, arr = []; i < this._cards.length; i++) {
-				var j = '',
-					index = 0,
-					newArr = ['Сесть на', 'Рейс №', 'из', 'в', 'место'];
+        /**
+         * Каждый объект массива _cards преобразует в строк и пушит в новый массив 
+         * @return {Array} - возвращает преобразованный массив arr 
+         */
+        conversion: function() {
+            for (var i = 0, arr = []; i < this._cards.length; i++) {
+                var j = '',
+                    index = 0,
+                    newArr = ['Сесть на', 'Рейс №', 'из', 'в', 'место'];
 
-				for (var key in this._cards[i]) {
-					j += ' ' + newArr[index] + ' ' + this._cards[i][key];
-					index++;
-				}
+                for (var key in this._cards[i]) {
+                    j += ' ' + newArr[index] + ' ' + this._cards[i][key];
+                    index++;
+                }
 
-				arr.push(j);
-			}
+                arr.push(j);
+            }
 
-			return arr;
-		}
-	}
+            return arr;
+        }
+    }
 
-	window.SCT = {
-		Cards: Cards,
-	}
+    window.SCT = {
+        Cards: Cards,
+    }
 }());
--- js/main.js
+++ js/main.js
@@ -2,55 +2,55 @@
 var btn = document.querySelector('#demo');
 btn.addEventListener('click', function() {
 
-	SCT.Cards.empty();
+    SCT.Cards.empty();
 
-	/**
-	 * Добавляем карточки в массив _cards
-	 */
-	SCT.Cards.addCard({typeTC: 'Автобус'      , flight: '2A' , seat: '15'       , A: 'Новочеркасск', B: 'Ростов' });
-	SCT.Cards.addCard({typeTC: 'Поезд'        , flight: '105', seat: 'свободное', A: 'Ростов'      , B: 'Шахты'  });
-	SCT.Cards.addCard({typeTC: 'Автобус'      , flight: '82Т', seat: '23'       , A: 'Шахты'       , B: 'Воронеж'});
-	SCT.Cards.addCard({typeTC: 'Автобус'      , flight: '12Б', seat: '1'        , A: 'Воронеж'     , B: 'Москва' });
-	SCT.Cards.addCard({typeTC: 'Ковер-Самолет', flight: '0'  , seat: 'свободное', A: 'Москва'      , B: 'Багдад' });
-	SCT.Cards.addCard({typeTC: 'Поезд'        , flight: '689', seat: '27'       , A: 'Багдад'      , B: 'АД'     });
+    /**
+     * Добавляем карточки в массив _cards
+     */
+    SCT.Cards.addCard({typeTC: 'Автобус'      , flight: '2A' , seat: '15'       , A: 'Новочеркасск', B: 'Ростов' });
+    SCT.Cards.addCard({typeTC: 'Поезд'        , flight: '105', seat: 'свободное', A: 'Ростов'      , B: 'Шахты'  });
+    SCT.Cards.addCard({typeTC: 'Автобус'      , flight: '82Т', seat: '23'       , A: 'Шахты'       , B: 'Воронеж'});
+    SCT.Cards.addCard({typeTC: 'Автобус'      , flight: '12Б', seat: '1'        , A: 'Воронеж'     , B: 'Москва' });
+    SCT.Cards.addCard({typeTC: 'Ковер-Самолет', flight: '0'  , seat: 'свободное', A: 'Москва'      , B: 'Багдад' });
+    SCT.Cards.addCard({typeTC: 'Поезд'        , flight: '689', seat: '27'       , A: 'Багдад'      , B: 'АД'     });
 
-	/**
-	 * Перетасовываем карточки
-	 */
-	SCT.Cards.shuffle();
+    /**
+     * Перетасовываем карточки
+     */
+    SCT.Cards.shuffle();
 
-	/**
-	 * Выводим список карточек до сортировки
-	 * В качестве аргумента функци передаем id списка в котором будем выводить
-	 */
-	createLi(before);
+    /**
+     * Выводим список карточек до сортировки
+     * В качестве аргумента функци передаем id списка в котором будем выводить
+     */
+    createLi(before);
 
-	/**
-	 * Сортируем карточки начиная с начала маршрута
-	 */
-	SCT.Cards.sortCards();
+    /**
+     * Сортируем карточки начиная с начала маршрута
+     */
+    SCT.Cards.sortCards();
 
-	/**
-	 * Выводим список карточек после сортировки
-	 * В качестве аргумента функци передаем id списка в котором будем выводить
-	 */
-	createLi(after);
+    /**
+     * Выводим список карточек после сортировки
+     * В качестве аргумента функци передаем id списка в котором будем выводить
+     */
+    createLi(after);
 });
 
 function createLi(listId) {
 
-	// Удаляет все элементы  listItem в указанном listId списке
-	while ( listId.hasChildNodes() ) {
-		listId.removeChild(listId.firstChild);
-	}
+    // Удаляет все элементы  listItem в указанном listId списке
+    while ( listId.hasChildNodes() ) {
+        listId.removeChild(listId.firstChild);
+    }
 
-	// Конвертирует массив _cards 
-	var cards = SCT.Cards.conversion();
+    // Конвертирует массив _cards 
+    var cards = SCT.Cards.conversion();
 
-	// Создает элемент listItem и добавляет в список listId
-	for (var i = 0; i < cards.length; i++) {
-		var newLi = document.createElement('li');
-		newLi.innerHTML = cards[i];
-		listId.appendChild(newLi);
-	}
+    // Создает элемент listItem и добавляет в список listId
+    for (var i = 0; i < cards.length; i++) {
+        var newLi = document.createElement('li');
+        newLi.innerHTML = cards[i];
+        listId.appendChild(newLi);
+    }
 }
